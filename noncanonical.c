778359ce149b3100405836248af08a8f8c921ba7
Manuel <mccosta2147@gmail.com>
2024-04-15 14:54:55 +0100

    Set state machine added for reciever.

--- noncanonical.c
+++ noncanonical.c
@@ -13,6 +13,11 @@
 
 volatile int STOP=FALSE;
 
+typedef struct sm 
+{
+  char state;
+};
+
 int main(int argc, char** argv)
 {
     int fd,c, res;
@@ -50,7 +55,7 @@ int main(int argc, char** argv)
     newtio.c_lflag = 0;
 
     newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
-    newtio.c_cc[VMIN]     = 5;   /* blocking read until 5 chars received */
+    newtio.c_cc[VMIN]     = 1;   /* blocking read until 5 chars received */
 
     /*
     VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a
@@ -68,12 +73,111 @@ int main(int argc, char** argv)
     printf("New termios structure set\n");
 
 //    while (STOP==FALSE) {       /* loop for input */
-        res = read(fd,buf,255);   /* returns after 5 chars have been input */
-        buf[res]=0;               /* so we can printf... */
+        
+    struct sm set_rec;
+
+    unsigned char BCCrec, bufrec[5], BCC;
+    int send;
+
+    set_rec.state = Start;
+
+    switch (set_rec.state)
+    {
+      case Start:
+        res = read(fd, buf, 1);
+        buf[res]=0;
+        if (buf[0] == 0x5c)
+        {
+          bufrec[0] = 0x5c;
+          set_rec.state = Flag_rec;
+        }
+        else
+        {
+          set_rec.state == Start;
+        }
+        break;
+      
+      case Flag_rec:
+        res = read(fd, buf, 1);
+        if (buf[0] == 0x03)
+        {
+          bufrec[1] = 0x03;
+          set_rec.state = A_rec;
+        }
+        else if(buf[0]== 0x5c)
+        {
+          set_rec.state = Flag_rec;
+        }
+        else
+        {
+          set_rec.state = Start;
+        }
+        break;
+      
+      case A_rec:
+        res = read(fd, buf, 1);
+        if (buf[0] == 0x08)
+        {
+          bufrec[2] = 0x08;
+          set_rec.state = C_rec;
+        }
+        else if(buf[0] == 0x5c)
+        {
+          set_rec.state = Flag_rec;
+        }
+        else
+        {
+          set_rec.state = Start;
+        }
+        break;
+      
+      case C_rec
+        res = read(fd, buf, 1);
+        if (buf[0] == (bufrec[1]^bufrec[2]))
+        {
+          bufrec[3] = buf[0];
+          set_rec.state = BCC_rec;
+        }
+        else if(buf[0] == 0x5c)
+        {
+          set_rec.state = Flag_rec;
+        }
+        else
+        {
+          set_rec.state = Start;
+        }
+        break;
+
+      case BCC_rec:
+        res = read(fd, buf, 1);
+        if (buf[0] == 0x5c)
+        {
+          bufrec[4] = 0x5c;
+          set_rec.state = Stop;
+        }
+        else
+        {
+          set_rec.state = Start;
+        }
+        break; 
+      
+      case Stop:
+        for (int i =0; i<5; i++)
+        {     
+            printf("0x%x\n", bufrec[i]);
+        }
+        bufrec[2] = 0x06;
+        bufrec[3] = 0x01^0x06;
+        send = write(fd, bufrec, 5);
+        printf("%d bytes written\n", send);
+        break;
+    }
+
+/*        res = read(fd,buf,255);    returns after 5 chars have been input 
+        buf[res]=0;                so we can printf... 
       
         
-      unsigned char BCCrec, bufrec[5], BCC;
-      int send;
+
 
       BCCrec = buf[1]^buf[2];  
      
@@ -98,9 +202,9 @@ int main(int argc, char** argv)
         printf("erro");
       }    
       
-    /*
+    
     O ciclo WHILE deve ser alterado de modo a respeitar o indicado no guião
-    */
+    */ 
     sleep(1);
     tcsetattr(fd,TCSANOW,&oldtio);
     close(fd);
